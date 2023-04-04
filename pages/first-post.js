1ffc76557de387fca06c6204062741360e554c08
Henrique Oliveira Gobi <102251338+HenriqueOgobi@users.noreply.github.com>
2023-04-04 11:20:00 -0300

    Changes

--- pages/first-post.js
+++ pages/first-post.js
@@ -1,26 +1,32 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import Head from 'next/head';
-import Script from 'next/script';
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from '../components/layout';
 
 export default function FirstPost() {
   return (
     <>
-    
-    <Head>
-        <title>First Post</title>
-        <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"//onLoad
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+
+      <Layout>
+        <Head>
+          <title>First Post</title>
+          <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload" //onLoad
+            onLoad={() =>
+              console.log(
+                `script loaded correctly, window.FB has been populated`
+              )
+            }
+          />
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </Layout>
+
     </>
   );
   const YourComponent = () => (
